c237cad505f15ecfbd38e002781f95203003295a
unamehere <niklas.krug.1@web.de>
2020-05-28 20:48:32 +0200

    new class for temperature measuring

---
+++ lib/TempMeasurement/tempmeasurement.cpp
@@ -0,0 +1,14 @@
+#include "tempmeasurement.h"
+
+tempmeasurement::tempmeasurement(/* args */)
+{
+}
+
+tempmeasurement::~tempmeasurement()
+{
+}
+
+uint16_t* tempmeasurement::getVals()
+{
+    return tempVals;
+}
