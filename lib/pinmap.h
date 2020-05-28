8c521d099dd1f74d188533a93f95f739b166d9fb
unamehere <niklas.krug.1@web.de>
2020-05-28 20:22:15 +0200

    added Pinmap to lib

---
+++ lib/pinmap.h
@@ -0,0 +1,19 @@
+
+//This File Contain all Pins of this Project
+
+#ifndef PINMAP
+#define PINMAP
+
+//MLX90614
+#define SCL_P 4
+#define SDA_P 16
+
+//Servo
+#define SERVO_TX_P 15
+#define SERVO_RX_P 2
+
+//Cam
+//todo
+
+
+#endif
