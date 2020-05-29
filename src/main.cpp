30d4365450743a4c5004a17629d90bd1af2f259a
Niklas <krug@hm.edu>
2020-05-29 16:36:01 +0200

    updated main to measure/test temp Measurement

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,19 @@
 #include <Arduino.h>
+#include "mainFunc/mainFunc.h"
 
 void setup() {
+  m_init();
   // put your setup code here, to run once:
 }
 
 void loop() {
+  long t1 = millis();
+  m_measure();
+  serialSendTemps();
+  int diff = millis()-t1;
+  Serial.print("Duration: ");
+  Serial.println(diff);
+  delay(1000);
+  
   // put your main code here, to run repeatedly:
 }
