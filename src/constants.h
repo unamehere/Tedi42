cc97c14afa92960879460a159a6667d400971581
Niklas <krug@hm.edu>
2020-06-01 16:47:06 +0200

    added constants

--- src/constants.h
+++ src/constants.h
@@ -7,6 +7,24 @@
 #define C_COMBAUD 115200
 #define C_SERVOBAUD 9600
 
+#define C_SERVOSPEED 512
+#define C_SERVODIFF 3
+
+#define MAXDEG_ROT 200
+#define MINDEG_ROT 10
+#define MAXDEG_TILT 200
+#define MINDEG_TILT 10
+
+#define ID_SERVO_ROT 1
+#define ID_SERVO_TILT 2
+
+#define COM_MAXLEN 6
+#define COM_GOTO_ROT        "GR"
+#define COM_GOTO_TILT       "GT"
+#define COM_MEASAT_ROT      "MR"
+#define COM_MEASAT_TILT     "MT"
+#define COM_MEASURE         "MM"
+
 
 
 #endif
