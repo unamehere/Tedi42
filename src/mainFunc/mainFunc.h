913a009c47d0d56f026894183b789995125b9d4e
Niklas <krug@hm.edu>
2020-06-01 16:47:54 +0200

    added serial comm handling definitions

--- src/mainFunc/mainFunc.h
+++ src/mainFunc/mainFunc.h
@@ -5,12 +5,19 @@
 #include "../MLX90621/MLX90621.h"
 #include <Arduino.h>
 
-
 // Main Functions
 bool m_init();
 bool initTemp();
 bool initServo();
 void serialSendTemps();
 void m_measure();
+void handleNewCommand();
+bool checkDeg(uint8_t ID, uint16_t const &deg);
+uint16_t degToInt(uint16_t &deg);
+
+bool getCommFlag();
+void setCommFlag(bool flag);
+
+String* getCommand();
 
 #endif
