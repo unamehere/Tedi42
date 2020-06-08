1d77e21174c3582051d82076f057a5bd777075ca
Niklas <krug@hm.edu>
2020-06-08 18:37:24 +0200

    removed unneccesary measure function

--- src/mainFunc/mainFunc.h
+++ src/mainFunc/mainFunc.h
@@ -10,7 +10,6 @@ bool m_init();
 bool initTemp();
 bool initServo();
 void serialSendTemps();
-void m_measure();
 void handleNewCommand();
 bool checkDeg(uint8_t ID, uint16_t const &deg);
 uint16_t degToInt(uint16_t &deg);
